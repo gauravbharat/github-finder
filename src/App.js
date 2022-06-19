2a0ad953dcb3a288bebda61158c30d7630cb8c4e
Gaurav Bhārat Mendse <gaurav.mendse@gmail.com>
2022-06-19 17:51:35 +0530

    HTTP requests and updating state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import React, { Fragment, Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import { Users } from "./components/users/Users";
+import Users from "./components/users/Users";
+import PropTypes from "prop-types";
+
+import axios from "axios";
+import { UserModel } from "./models/users.model";
 
 class App extends Component {
-  foo = () => "Bars";
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    let response = null;
+
+    try {
+      response = await axios.get(`https://api.github.com/users`);
+      console.log(response?.data);
+      this.setState({ users: response?.data, loading: false });
+    } catch (error) {
+      console.log({ error });
+      this.setState({ users: [], loading: false });
+    }
+  }
 
   render() {
+    const { loading, users } = this.state;
     return (
       <Fragment>
         <Navbar title="Garyd's Github Finder" />
         <div className="container">
-          <Users />
+          <Users loading={loading} users={users} />
         </div>
       </Fragment>
     );
   }
 }
+
+App.propTypes = {
+  user: PropTypes.arrayOf(UserModel),
+  loading: PropTypes.bool,
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("traversy"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
