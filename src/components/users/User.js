9c394f9efc607261cc21cb0ee56b047eb8648571
Gaurav Bhārat Mendse <gaurav.mendse@gmail.com>
2022-06-26 10:11:29 +0530

    Using useEffect to perform side effects in functional component

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -3,13 +3,26 @@
 import PropTypes from "prop-types";
 import { Spinner } from "../layout/spinner/Spinner";
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 const User = ({ loading, user, getUser, repos, getUserRepos }) => {
   const params = useParams();
 
   console.log({
     params,
+    user,
+    loading,
   });
+
+  /** The Effect Hook lets you perform side effects in function components
+   * Similar to componentDidMount and componentDidUpdate
+   * Since useEffect can go in endless loop after updating the component, pass on empty array to run it once
+   * It would give a warning to pass missing dependencies BUT it would cause useEffect to reload again in endless loop, disable eslint in that case for next line
+   */
+  useEffect(() => {
+    getUser(params.loginId);
+    //eslint-disable-next-line
+  }, []);
 
   //   if (!props.user) {
   //     await props.getUser(username);
